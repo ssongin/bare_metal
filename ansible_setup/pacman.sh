1ecdf98192d2e6728d305a321b3b3c933be36768
Stanislav <ssongin@gmail.com>
2024-06-22 12:58:54 +0300

    Add ansible file to display system info

--- ansible_setup/pacman.sh
+++ ansible_setup/pacman.sh
@@ -12,3 +12,9 @@ pipx install --include-deps ansible
 echo -e "\nConfiguring path for ansible\n"
 pipx ensurepath
 pipx completions
+
+echo -e "\nCreate \"Projects\" directory\n"
+mkdir ~/Projects
+
+echo -e "\nClone this (Bare metal) repo\n"
+git clone https://github.com/ssongin/bare_metal.git
